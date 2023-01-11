810a4175f413a09f170def9a6f12c31f86f9593a
Evelin <bellidoevelin@gmail.com>
2023-01-11 17:53:19 -0500

    cheatsheet for numbers

---
+++ numbers.js
@@ -0,0 +1,34 @@
+/*Math object cheat sheet*/
+
+//redondea al entero mas cercano.
+Math.ceil();
+//redondea abajo para el entero mas cerca
+Math.floor();
+//redondea al entero mas cercano hacia arriba si el decimal es .5 si es menos redondea hacia abajo.
+Math.round();
+//quita la parte de decimal de un numero dejando solo el entero.
+Math.trunc();
+
+/*--------------------------------------------------------------*/
+
+// Arithmetic and calculus methods
+//calcula la potencia 2 esel numero y 3 el exponente
+Math.pow(2,3);
+//calcula la raiz cuadrada de un numero
+Math.sqrt(16);
+//calcula la operacion exponencial de 8 en este caso seria 2.
+Math.cbrt(8)
+//finds the cube root of 8, the result is 2
+Math.cbrt(8);
+// returns the absolute value, the result is 10 
+Math.abs(-10) -
+
+// Logarithmic methods: 
+Math.log(), Math.log2(), Math.log10() 
+
+// Return the minimum and maximum values of all the inputs: 
+Math.min(9,8,7) 
+// returns 7, Math.max(9,8,7) returns 9.
+
+//  Trigonometric methods: 
+Math.sin(), Math.cos(), Math.tan()
